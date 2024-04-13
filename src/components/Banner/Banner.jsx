0a502142065eb62416b2e93ff3b924d6cd90756b
EMON <mamudmdemon@gmail.com>
2024-04-13 19:50:49 +0600

    carousel completed with responsive

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,46 +1,129 @@
-import { Carousel } from "@material-tailwind/react";
-
+import {  Button, Carousel, Typography } from "@material-tailwind/react";
 
 const Banner = () => {
   return (
     <div>
-       <Carousel
-       transition={{ duration: 1.5 }} 
-      
-      navigation={({ setActiveIndex, activeIndex, length }) => (
-        <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
-          {new Array(length).fill("").map((_, i) => (
-            <span
-              key={i}
-              className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
-                activeIndex === i ? "w-8 bg-blue-900" : "w-4 bg-white/50"
-              }`}
-              onClick={() => setActiveIndex(i)}
-            />
-          ))}
+      <Carousel transition={{ duration: 1.5}}>
+      <div className="relative h-full w-full">
+        <img
+          src="https://i.ibb.co/R7N4BSR/beach-front.jpg"
+          alt="image 1"
+          className=" w-full h-[250px] md:h-[300px] lg:h-[835px] object-cover"
+        />
+        <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
+          <div className="w-3/4 text-center md:w-2/4">
+            <Typography
+              variant="h1"
+              color="white"
+              className="mb-1 md:mb-3 lg:mb-4 text-sm md:text-base lg:text-xl"
+            >
+              Good Service is our passion
+            </Typography>
+            <Typography
+              variant="h1"
+              color="white"
+              className="mb-2 md:mb-3 lg:mb-4 text-2xl md:text-3xl lg:text-4xl"
+            >
+              Awesome apartment Villa
+            </Typography>
+            <Typography
+              variant="lead"
+              color="white"
+              className="mb-4 md:mb-4 lg:mb-12 opacity-80 text-xs md:text-base lg:text-lg"
+            >
+              No matter what the weather, no matter what the situation we are in, if we have the right perspective in life, life will always be beautiful!
+            </Typography>
+            <div className="flex justify-center gap-2 ">
+              <Button size="lg" color="white" className="hidden lg:block">
+                Explore
+              </Button>
+              <Button size="lg" color="white" variant="text">
+                Gallery
+              </Button>
+            </div>
+          </div>
         </div>
-      )}
-    >
-      <img
-        src="https://i.ibb.co/H2VJynz/a054ac26.jpg"
-        alt="image 1"
-        className=" w-full h-[200px] lg:h-[835px] object-cover"
-      />
-      <img
-        src="https://i.ibb.co/2PqCKC9/seaside-villas.jpg"
-        alt="image 2"
-        className=" w-full h-[200px] lg:h-[835px] object-cover"
-      />
-      <img
-        src="https://i.ibb.co/KF6G3Tx/mountaine-Chalet.jpg"
-        alt="image 3"
-        className="  w-full h-[200px] lg:h-[835px] object-cover"
-      />
-      <img
-        src="https://i.ibb.co/cJZQNmC/urban-boutique-hotel.jpg"
-        alt="image 3"
-        className="  w-full h-[200px] lg:h-[835px] object-cover"
-      />
+      </div>
+      <div className="relative h-full w-full">
+        <img
+          src="https://i.ibb.co/KF6G3Tx/mountaine-Chalet.jpg"
+          alt="image 2"
+          className=" w-full h-[250px] md:h-[300px] lg:h-[835px] object-cover"
+        />
+        <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
+          <div className="w-3/4 text-center md:w-2/4">
+            <Typography
+              variant="h1"
+              color="white"
+              className="mb-1 md:mb-3 lg:mb-4 text-sm md:text-base lg:text-xl"
+            >
+              Good Service is our passion
+            </Typography>
+            <Typography
+              variant="h1"
+              color="white"
+              className="mb-2 md:mb-3 lg:mb-4 text-2xl md:text-3xl lg:text-4xl"
+            >
+              Awesome apartment Villa
+            </Typography>
+            <Typography
+              variant="lead"
+              color="white"
+              className="mb-4 md:mb-4 lg:mb-12 opacity-80 text-xs md:text-base lg:text-lg"
+            >
+              No matter what the weather, no matter what the situation we are in, if we have the right perspective in life, life will always be beautiful!
+            </Typography>
+            <div className="flex justify-center gap-2">
+              <Button size="lg" color="white">
+                Explore
+              </Button>
+              <Button size="lg" color="white" variant="text">
+                Gallery
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="relative h-full w-full">
+        <img
+          src="https://i.ibb.co/NmNdVtc/safari-lodge.jpg"
+          alt="image 3"
+          className=" w-full h-[250px] md:h-[300px] lg:h-[835px] object-cover"
+        />
+        <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
+          <div className="w-3/4 text-center md:w-2/4">
+            <Typography
+              variant="h1"
+              color="white"
+              className="mb-1 md:mb-3 lg:mb-4 text-sm md:text-base lg:text-xl"
+            >
+              Good Service is our passion
+            </Typography>
+            <Typography
+              variant="h1"
+              color="white"
+              className="mb-2 md:mb-3 lg:mb-4 text-2xl md:text-3xl lg:text-4xl"
+            >
+              Awesome apartment Villa
+            </Typography>
+            <Typography
+              variant="lead"
+              color="white"
+              className="mb-4 md:mb-4 lg:mb-12 opacity-80 text-xs md:text-base lg:text-lg"
+            >
+              No matter what the weather, no matter what the situation we are in, if we have the right perspective in life, life will always be beautiful!
+            </Typography>
+            <div className="flex justify-center gap-2">
+              <Button size="lg" color="white">
+                Explore
+              </Button>
+              <Button size="lg" color="white" variant="text">
+                Gallery
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </Carousel>
     </div>
   );
