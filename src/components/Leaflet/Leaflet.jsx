96fcb40185383462487d7442005775a65cadc7f5
umayermdemon <mamudmdemon@gmail.com>
2025-07-05 11:48:43 +0600

    fix: add all

--- src/components/Leaflet/Leaflet.jsx
+++ src/components/Leaflet/Leaflet.jsx
@@ -3,21 +3,23 @@ import "leaflet/dist/leaflet.css";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 const Leaflet = () => {
-  
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true} className="leaflet-container z-0">
-    
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={[51.505, -0.09]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={true}
+      className="leaflet-container z-0"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
-    
 };
 export default Leaflet;
